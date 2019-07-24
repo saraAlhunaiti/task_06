2f414f859ac81b3defebe0b029c1da50db0d1f74
saraAlhunaiti <alhunaitisara@gmail.com>
2019-07-24 17:26:31 +0300

    Day 4 task_06 -Template Inheritance-

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -4,4 +4,11 @@ from .models import Restaurant
 class RestaurantForm(forms.ModelForm):
 	class Meta:
 		model =Restaurant
-		fields=['name','description','opening_time','closing_time']
+		# fields=['name','description','opening_time','closing_time']
+		fields= '__all__'
+
+		widgets ={
+			"opening_time" : forms.TimeInput(attrs={"type":"time"}),
+			"closing_time" : forms.TimeInput(attrs={"type":"time"})
+
+		}
